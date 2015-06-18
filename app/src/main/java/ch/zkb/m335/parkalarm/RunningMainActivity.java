e23fc9d113c236c7121f22185260e38e5dcb6583
Office-R <hundsjfauosdf@4fasdf.ch>
2015-06-18 11:05:35 +0200

    Neue Bugs imlementiert

--- app/src/main/java/ch/zkb/m335/parkalarm/RunningMainActivity.java
+++ app/src/main/java/ch/zkb/m335/parkalarm/RunningMainActivity.java
@@ -118,7 +118,7 @@ public class RunningMainActivity extends Activity {
         new AlertDialog.Builder(this)
             .setTitle(getString(R.string.stop_timer_popup_title))
             .setMessage(getString(R.string.stop_timer_popup_message))
-            .setIcon(android.R.drawable.ic_dialog_alert)
+                .setIcon(R.mipmap.ic_parking)
             .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
                     goToMain();
