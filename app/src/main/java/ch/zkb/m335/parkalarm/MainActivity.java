3ffc65fdc7df032bb010177a28b5e1c6889cf687
grosssven <gross.sven@hotmail.com>
2015-06-18 10:48:36 +0200

    diverses

--- app/src/main/java/ch/zkb/m335/parkalarm/MainActivity.java
+++ app/src/main/java/ch/zkb/m335/parkalarm/MainActivity.java
@@ -35,6 +35,11 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
