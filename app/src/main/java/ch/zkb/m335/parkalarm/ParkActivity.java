9d2b635341eee9e554272f7779a8407c409ab619
grosssven <gross.sven@hotmail.com>
2015-06-19 08:41:52 +0200

    darstellung

--- app/src/main/java/ch/zkb/m335/parkalarm/ParkActivity.java
+++ app/src/main/java/ch/zkb/m335/parkalarm/ParkActivity.java
@@ -125,7 +125,7 @@ public class ParkActivity extends FragmentActivity {
                 }
 
                 //Get location from NETWORK_PROVIDER -> permission in manifest!
-                MyLocationListener locationListener = new MyLocationListener();
+                LocationListener locationListener = new MyLocationListener();
                 LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
                 locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, locationListener);
                 Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
